0cd06dcae1fdbb82d37f6154cb62490ddac680fd
Akradabas <akradabaska@gmail.com>
2020-11-22 21:53:27 -0800

    added more katas

--- Katas.js
+++ Katas.js
@@ -167,3 +167,460 @@ function roots(a,b,c){
 
 }
 print roots(a, b, c);
+
+// switch is the keyword, n is the variable to switch, case 1 means when n===1,
+//     following the ":",you can add some executable statement, with keyword
+//     break as termination(if you forgot "break", code will running
+//     with case 2, case 3...until a "break" appear),default like else in the if..else statement.
+
+function howManydays(month){
+    var days;
+    switch (month){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+        case 2:
+            return 28;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+    }
+    return days;
+}
+
+// You are required to create a simple calculator that returns the result of addition,
+// subtraction, multiplication or division of two numbers.
+// Your function will accept three arguments:
+// The first and second argument should be numbers.
+// The third argument should represent a sign indicating the operation to perform on these two numbers.
+// if the variables are not numbers or the sign does not belong to the list above a
+// message "unknown value" must be returned.
+
+function calculator(a,b,sign){
+    switch (sign) {
+        case '+':
+            if (typeof a === 'number' && typeof b === 'number') return a + b
+            return  "unknown value";
+        case '-':
+            if (typeof a === 'number' && typeof b === 'number') return a - b
+            return  "unknown value";
+        case '*':
+            if (typeof a === 'number' && typeof b === 'number') return a * b
+            return  "unknown value";
+        case '/':
+            if (typeof a === 'number' && typeof b === 'number' && b !== 0) return a / b
+            return  "unknown value";
+        default:
+            return "unknown value";
+    }
+}
+
+// Даны два положительных целых числа n и k. Используя только операции сложения и вычитания,
+//     найдите результат деления числа n на число k (частное), а также остаток от деления.
+//     Напишите функцию с именем quotientAndRemainder, которая принимает
+// два числа n, k и возвращает частное и остаток от деления в виде массива [quotient, remainder].
+// функция quotientAndRemainder(5, 5) должна возвратить [1, 0];
+// функция quotientAndRemainder(5, 2) должна возвратить [2, 1];
+// функция quotientAndRemainder(12, 5) должна возвратить [2, 2];
+// функция quotientAndRemainder(5, 7) должна возвратить [0, 5]
+
+function quotientAndRemainder(n, k) {
+    var quotient = 0;
+    while (n > k) {
+        n -= k;
+        quotient++;
+        if (n == k) {
+            quotient++;
+            n -= k;
+        }
+    }
+    var remainder = n;
+    return [quotient, remainder];
+}
+
+// Your task, is to create NxN multiplication table, of size provided in parameter.
+//
+//     for example, when given size is 3:
+//
+// 1 2 3
+// 2 4 6
+// 3 6 9
+// for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+
+multiplicationTable = function(size) {
+    let arr = [];
+    for (let i = 1; i <= size; i++) {
+        let str = [];
+        for(let y = 1; y <= size; y++) {
+            str.push(y*i);
+        }
+        arr.push(str);
+    }
+    return arr;
+}
+
+//
+// Task:
+//     You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+//     Note:Returning the pattern is not the same as Printing the pattern.
+//     Rules/Note:
+// If n < 1 then it should return "" i.e. empty string.
+//     There are no whitespaces in the pattern.
+// pattern(5):
+//
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+
+function pattern(n){
+    var output="";
+    for(let i = 1; i <= n; i++) {
+        output+= String(i).repeat(i)+ '\n';
+    }
+    return output.slice(0, -1);
+}
+
+// Task:
+//     You have to write a function pattern which returns the following Pattern (See Pattern & Examples) upto n number of rows.
+//
+//     Note: Returning the pattern is not the same as Printing the pattern.
+//     Rules/Note:
+// If n < 1 then it should return "" i.e. empty string.
+//     There are no whitespaces in the pattern.
+//
+// pattern(4):
+//
+// 4321
+// 432
+// 43
+// 4
+
+function pattern(n){
+    let output="";
+    let str = [];
+    for (let i = n; i >= 1; i--) {
+        str.push(i);
+    }
+    while (str.length > 0) {
+        output+= str.join('')+'\n';
+        str = str.slice(0, -1);
+    }
+    return output.slice(0, -1);
+}
+
+// Complete the function power_of_two/powerOfTwo (or equivalent,
+//     depending on your language) that determines
+// if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+//
+//     a power of two is a number of the form 2n where n is an integer,
+//     i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+//
+//     You may assume the input is always valid.
+//     Examples
+// isPowerOfTwo(1024) // -> true
+// isPowerOfTwo(4096) // -> true
+// isPowerOfTwo(333)  // -> false
+// Beware of certain edge cases - for example, 1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.
+
+function isPowerOfTwo(n){
+    if (Number.isInteger(Math.log2(n)))   return true;
+    return false;
+}
+
+
+// Дано целое положительное число n, которое равно 2, возведенной в некоторую степень k:
+// Найдите эту степень (число k).
+// Напишите функцию с именем powerOf2, которая принимает число n в качестве аргумента и
+// возвращает число k, такое, что 2 в степени k равно n. При решении используйте цикл while и
+// операции деления или умножения. Запрещается использовать методы объекта
+
+function powerOf2(n){
+    let k = 0;
+    while (n > 1) {
+        n = n / 2;
+        k = k + 1;
+    }
+    return k;
+}
+
+
+// Дано целое положительное число n. Найдите факториал числа n (n! = 1 * 2 * 3 *... * n)
+// //
+// // Напишите функцию с именем factorial, которая принимает число n в качестве аргумента и
+// // возвращает факториал числа n, т.е. произведение чисел от 1 до n. При решении используйте цикл while.
+// //
+// // Примеры:
+// //
+// //     функция factorial(2) должна возвратить 2;
+// // функция factorial(3) должна возвратить 6;
+// // функция factorial(8) должна возвратить 40320
+
+function factorial(n){
+    let fuc = 1;
+    while (n > 0) {
+        fuc = fuc * n;
+        n -= 1;
+    }
+    return fuc;
+}
+
+
+// Найдите сумму нечетных чисел от 1 до n, т.е. 1 + 3 + 5 + ...
+//
+// Напишите функцию с именем sumOfOdd, которая принимает число n в качестве
+// аргумента и возвращает cумму нечетных чисел от 1 до n. При решении используйте цикл while.
+//
+// Примеры:
+//
+// функция sumOfOdd(1) должна возвратить 1;
+// функция sumOfOdd(3) должна возвратить 4;
+// функция sumOfOdd(20) должна возвратить 100.
+
+function sumOfOdd(n){
+    let i = 1;
+    let sum = 0;
+    while (n > 0 && i <= n) {
+        sum += i;
+        i = i + 2;
+    }
+    return sum;
+}
+
+
+// Найдите произведение четных чисел от 2 до n, т.е. 2 * 4 * 6 * ...
+//
+// Напишите функцию с именем multOfEven, которая принимает число n в качестве аргумента
+// и возвращает произведение четных чисел от 2 до n. При решении используйте цикл while.
+//
+// Примеры:
+//
+// функция multOfEven(2) должна возвратить 2;
+// функция multOfEven(6) должна возвратить 48;
+// функция multOfEven(10) должна возвратить 3840.
+
+function multOfEven(n){
+    let i = 2;
+    let s = 1;
+    while (n > 0 && i <= n) {
+        s = s * i;
+        i = i + 2;
+    }
+    return s;
+}
+
+// Напишите функцию с именем sumOfDigits, которая принимает неотрицательное
+// число n (n>=0) в качестве аргумента и возвращает cумму цифр этого числа.
+// При решении используйте цикл while. Запрещено использовать методы строк и массивов.
+//
+//     Примеры:
+//
+// функция sumOfDigits(25) должна возвратить 7;
+// функция sumOfDigits(658) должна возвратить 19;
+// функция sumOfDigits(13881) должна возвратить 21
+
+function sumOfDigits(n) {
+    let sum = 0;
+    while (n != 0) {
+        sum = sum + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return sum;
+}
+
+// Напишите функцию с именем numberOfDigits, которая принимает неотрицательное
+// число n (n>=0) в качестве аргумента и возвращает количество цифр этого числа.
+// При решении используйте цикл while. Запрещено использовать методы и свойства строк и массивов.
+//
+//     Примеры:
+//
+// функция numberOfDigits(2) должна возвратить 1;
+// функция numberOfDigits(63) должна возвратить 2;
+// функция numberOfDigits(10123) должна возвратить 5
+
+function numberOfDigits(n){
+    let count = 0;
+    while (n > 1) {
+        n = n / 10;
+        count += 1;
+    }
+    return count;
+}
+
+// Ученик к моменту начала обучения не знает ни одного английского слова.
+// В первый день занятий он выучил 5 английских слов. В каждый последующий
+// день он выучивал на 2 слова больше, чем в предыдущий. Через сколько дней ученик будет
+// знать не менее n английских слов?
+// Напишите функцию с именем englishWords, которая принимает в качестве аргумента неотрицательное
+// число n (n>=0) (количество английских слов, которые хочет выучить ученик) и возвращает
+// количество дней, которые необходимы для изучения n слов.
+//     Примеры:
+// функция englishWords(5) должна возвратить 1; // 5 слов ученик может выучить за 1 день.
+// функция englishWords(10) должна возвратить 2; // 10 слов ученик может выучить за 2 дня (5 + 7 = 12)
+// функция englishWords(20) должна возвратить 3. // 20 слов ученик может выучить за 3 дня (5 + 7 + 9 = 21)
+// функция englishWords(30) должна возвратить 4. // 30 слов ученик может выучить за 4 дня (5 + 7 + 9 + 11 = 32)
+
+function englishWords(n) {
+    let days = 0;
+    let words = 5;
+    let totalWords = 0
+    while (totalWords < n) {
+        totalWords += words;
+        words = words + 2;
+        days++;
+    }
+    return days
+}
+
+
+// Recreation of Project Euler problem #6
+// Find the difference between the sum of the squares of the
+// first n natural numbers (1 <= n <= 100) and the square of their sum.
+//     Example
+// For example, when n = 10:
+// The square of the sum of the numbers is:
+// (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)2 = 552 = 3025
+// The sum of the squares of the numbers is:
+// 12 + 22 + 32 + 42 + 52 + 62 + 72 + 82 + 92 + 102 = 385
+// Hence the difference between square of the sum of the first ten natural numbers
+// and the sum of the squares of those numbes is: 3025 - 385 = 2640
+
+function differenceOfSquares(n){
+    let squareOfSum = 0;
+    let sumOfSquare = 0;
+    let sum = 0;
+    while( n >= 0 && n <= 100) {
+        sum = (n * (n + 1)) / 2;
+        sumOfSquare = (n * (n + 1) * (2 * n + 1)) / 6;
+        squareOfSum = sum * sum;
+        return squareOfSum - sumOfSquare;
+
+    }
+}
+
+// My friend John likes to go to the cinema. He can choose between system A and system B.
+//
+//     System A : he buys a ticket (15 dollars) every time
+// System B : he buys a card (500 dollars) and a first ticket for 0.90 times the ticket price,
+//     then for each additional ticket he pays 0.90 times the price paid for the previous ticket.
+//     #Example: If John goes to the cinema 3 times:
+//
+//     System A : 15 * 3 = 45
+// System B : 500 + 15 * 0.90 + (15 * 0.90) * 0.90 + (15 * 0.90 * 0.90) * 0.90 ( = 536.5849999999999, no rounding for each ticket)
+// John wants to know how many times he must go to the cinema so that the final
+// result of System B, when rounded up to the next dollar, will be cheaper than System A.
+//
+//     The function movie has 3 parameters: card (price of the card), ticket
+// (normal price of a ticket), perc (fraction of what he paid for the previous ticket) and returns the first n such that
+//
+// ceil(price of System B) < price of System A.
+
+function movie(card, ticket, perc) {
+    var costA = n = 0,
+        costB = card;
+    while (Math.ceil(costB) >= costA) {
+        costA += ticket;
+        costB += ticket * Math.pow(perc,++n);
+    }
+    return n;
+};
+
+// Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know
+// // how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+// // The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly.
+// //     After paying taxes 'T' for the year the new sum is re-invested.
+// //     Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+function calculateYears(principal, interest, tax, desired) {
+    let years = 0;
+    while (principal < desired) {
+        let gains = principal * interest - (principal * interest * tax);
+        principal += gains;
+        years ++;
+    }
+    return years;
+}
+
+// Task
+// Write a method remainder which takes two integer arguments,
+//     dividend and divisor, and returns the remainder when dividend is divided by divisor.
+//     Do NOT use the modulus operator (%) to calculate the remainder!
+//     Assumption
+// Dividend will always be greater than or equal to divisor.
+//     Notes
+// Make sure that the implemented remainder function works exactly the same as the Modulus operator (%).
+
+function remainder(D, d) {
+    return D - Math.trunc(D / d) * d
+}
+
+
+// Numbers ending with zeros are boring.
+//
+//     They might be fun in your world, but not here.
+//
+//     Get rid of them. Only the ending ones.
+//
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+
+function noBoringZeros(n) {
+    return Number(`${n}`.replace(/0+$/, ''));
+}
+
+// Yor task is to write function factorial
+
+function factorial(n){
+    let f = 1;
+    if (n === 1 || n === 0) {
+        return 1;
+    } else {
+        for (let i = n; i >= 1;i--) {
+            f = f * i
+        }
+        return f;
+    }
+}
+
+//
+// Given a positive integer N, return the largest integer k such that 3^k < N.
+//     For example,
+// largest_power(3) == 0
+// largest_power(4) == 1
+// You may assume that the input to your function is always a positive integer.
+
+    function largestPower(n){
+    return Math.ceil(Math.log10(n) / Math.log10(3)) - 1;
+}
+
+// Description:
+//     #Task:
+// Write a function that returns true if the number is a "Very Even" number.
+//     If a number is a single digit, then it is simply "Very Even" if it itself is even.
+//     If it has 2 or more digits, it is "Very Even" if the sum of it's digits is "Very Even".
+// #Examples:
+// input(88) => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd
+// input(222) => returns true
+// input(5) => returns false
+// input(841) => returns true -> 8 + 4 + 1 = 13 -> 1 + 3 => 4 is even
+// Note: The numbers will always be 0 or positive integers!
+
+function isVeryEvenNumber(n) {
+    if ((n - 1) % 9 % 2 != 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
